6b0f6d67ba3af26d3df55d6fa8b439e204aca978
ruidinis <ruidinis@sapo.pt>
2021-03-25 11:46:27 +0000

    update_25-03-2021

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = new express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
+
+function getNLUInstance() {
+  let api_key = process.env.API_KEY;
+  let api_url = process.env.API_URL;
+
+  console.log(api_url)
+
+  const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+  const { IamAuthenticator } = require('ibm-watson/auth');
+
+  const NaturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+    version: '2020-08-01',
+    authenticator: new IamAuthenticator({
+      apikey: api_key,
+    }),
+    serviceUrl: api_url,
+  });
+
+  return NaturalLanguageUnderstanding;
+}
+
 
 app.get("/",(req,res)=>{
     res.render('index.html');
